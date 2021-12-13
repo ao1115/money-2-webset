d753509fdddffbdddd2f2b44b59231688806a89f
ao1115 <1677120510@qq.com>
2021-12-13 23:37:27 +0800

    deploy

---
+++ precache-manifest.386e4021585e99f994375e09958494f3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61003801c148262f750a",
+    "url": "/money-2-webset/css/app.6db7a44c.css"
+  },
+  {
+    "revision": "0f3023940c3fa39a259b",
+    "url": "/money-2-webset/css/chunk-vendors.a16c4353.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/money-2-webset/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/money-2-webset/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "407486ca2a000b6bde10f5e32e67aa55",
+    "url": "/money-2-webset/index.html"
+  },
+  {
+    "revision": "61003801c148262f750a",
+    "url": "/money-2-webset/js/app.52b534c2.js"
+  },
+  {
+    "revision": "0f3023940c3fa39a259b",
+    "url": "/money-2-webset/js/chunk-vendors.bde33a78.js"
+  },
+  {
+    "revision": "61949b713f1c54328abac44d7552ce8a",
+    "url": "/money-2-webset/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-2-webset/robots.txt"
+  }
+]);
